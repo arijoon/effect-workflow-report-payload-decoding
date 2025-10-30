0b71d4254778b9f76c5fee05d2d9c8f6c768c332
Arman Yaraee <arman.yaraee@deversifi.com>
2025-10-30 14:32:43 +0000

    Init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Typescript project";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs"; # use nix registry nixpkgs
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        packages.default = pkgs.hello;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pnpm
+            dprint
+          ];
+        };
+      });
+}
